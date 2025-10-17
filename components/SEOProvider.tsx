299117993adf9ab15e3bd8823262b1c247e2e47f
mbiti001e <mbitiedwin@gmail.com>
2025-10-17 23:43:40 +0300

    Remove next-seo dependency for app router compatibility

--- components/SEOProvider.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { DefaultSeo } from 'next-seo'
-import defaultSeoConfig from '../next-seo.config'
-
-export default function SEOProvider() {
-  return <DefaultSeo {...defaultSeoConfig} />
-}
